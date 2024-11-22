fbd6d4a42887729327fb64f2f0cd734ba1c2abec
minh-115 <minhquangpham502@gmail.com>
2024-11-22 17:32:58 +0700

    FT-02: Sửa giao diện Mud Blazor

--- BlazorApp/BlazorApp/Program.cs
+++ BlazorApp/BlazorApp/Program.cs
@@ -2,11 +2,9 @@
 using BlazorApp.Data;
 using BlazorApp.Repositories.ProductRepository;
 using BlazorApp.StoredProcedureHepler;
-using Grpc.Net.Client;
 using GrpcServer;
 using Microsoft.EntityFrameworkCore;
-using MudBlazor;
-
+using Microsoft.Extensions.Hosting;
 using MudBlazor.Services;
 var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddRazorPages();
@@ -14,18 +12,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddMudServices();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IStoredProcedureHelper, StoredProcedureHelper>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
-//builder.Services.AddScoped(sp =>
-//{
-//    var grpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
-//    return new Greeter.GreeterClient(grpcChannel);
-//});
+
 //commit program.cs
+builder.Services.AddMudServices();
 builder.Services.AddRazorPages();
 builder.Services.AddGrpcClient<Greeter.GreeterClient>(options =>
 {
