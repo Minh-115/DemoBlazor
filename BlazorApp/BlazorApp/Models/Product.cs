fcff55751421a894b7a7d7ac7e96b2ba1d70adcb
minh-115 <minhquangpham502@gmail.com>
2024-11-26 10:09:25 +0700

    test Endpoint ở appsetting.json

--- BlazorApp/BlazorApp/Models/Product.cs
+++ BlazorApp/BlazorApp/Models/Product.cs
@@ -1,12 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorApp.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Product Name is required")]   
         public string Name { get; set; } = "";
+        [Required(ErrorMessage = "Price is required")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "Quantity is required")]
         public int Quantity { get; set; }
         [NotMapped]                
         public int totalRows { get; set; }        
