213ac7f5ba2fff6fee8a3ea9881067258d6f5cb9
Minh-115 <minhquangpham502@gmail.com>
2025-09-06 16:29:34 +0700

    áÁa

--- BlazorApp/WEB/Constant/Endpoints.cs
+++ BlazorApp/WEB/Constant/Endpoints.cs
@@ -4,6 +4,6 @@
     {
         public static string PRODUCT_GETBYID = "products/GetById";
         public static string PRODUCT_GETALL = "products/GetALL";
-        public static string PRODUCT_UPDATE = "products/UPDATE";
+        public static string PRODUCT_UPDATE = "products/UPDATE";/////
     }
 }
